704ab2018af78d2158da98d6e903bddb069b0027
skyant83 <skyant83@gmail.com>
2025-04-22 21:40:39 +0800

    Separated the evac-center information into its own .json file
    Created a Custom Evacuation map pin svg
    
    Adjusted the Evac Pin Popup HTML and location

--- js/index.js
+++ js/index.js
@@ -14,46 +14,17 @@ import {
   collection,
   getDocs,
 } from 'https://www.gstatic.com/firebasejs/9.18.0/firebase-firestore.js';
+import evacCenters from '/hardcode/evac-centers.json' with {type: 'json'};
 
 var colRef = getCollection();
 
 map.panTo(new L.LatLng(14.673, 121.11215));
 
-const evacuationCenters = [
-  {
-    "name": "Ynares Basketball Court",
-    "latitude": 14.676018,
-    "longitude": 121.109596,
-  },
-  {
-    "name": "SM San Mateo",
-    "latitude": 14.679451,
-    "longitude": 121.114464,
-  },
-  {
-    "name": "Buklod Tao",
-    "latitude": 14.671060,
-    "longitude": 121.126207,
-  }
-];
-
-evacuationCenters.forEach(center => {
-  const marker = L.marker([center.latitude, center.longitude], {
-    icon: L.divIcon({
-      className: 'evac-marker'
-    })
-  }).addTo(map);
-
-  marker.bindPopup(`
-    <div class = "evac-marker-header">EVACUATION CENTER</div>
-    <b>${center.name}</b>
-    <br>Location: ${center.latitude}, ${center.longitude}`);
-});
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+  }).addTo(map);
 
 var searchControl = L.esri.Geocoding.geosearch().addTo(map);
 
@@ -195,7 +166,7 @@ getDocs(colRef)
     querySnapshot.forEach((entry) => {
       var doc = entry.data();
       var marker = L.marker([0, 0]);
-      //console.log(doc);
+
       if (doc.location_coordinates != null) {
         marker = L.marker([
           parseFloat(doc.location_coordinates._lat),
@@ -206,26 +177,40 @@ getDocs(colRef)
       var popupContent = onPinClick(doc);
       marker.bindPopup(popupContent);
       marker.on('popupopen', function(e) {
-        console.log('open');
-
         var editBtn = document.getElementById('editHouseholdPopup')
         if (editBtn) {
-            editBtn.addEventListener('click', function() {
-              const modal = document.getElementById('partnerModal');
-              var editFormModal = document.getElementById('editModal');
-              editFormModal.style.display = 'block';
-              modal.style.display = 'none';
-              console.log(doc.household_name);
-              populateEditForm(doc, editFormModal)
-            })
-          }
-        })
-        results.addLayer(marker);
+          editBtn.addEventListener('click', function() {
+            const modal = document.getElementById('partnerModal');
+            var editFormModal = document.getElementById('editModal');
+            editFormModal.style.display = 'block';
+            modal.style.display = 'none';
+            populateEditForm(doc, editFormModal)
+          })
+        }
+      })
+      results.addLayer(marker);
     });
-  })
-  .catch((error) => {
+  }).catch((error) => {
     console.error('Error getting documents: ', error);
   });
+
+evacCenters.forEach(center => {
+  const marker = L.marker(
+    [center.latitude, center.longitude],
+    {icon: L.icon({
+      iconUrl: "/hardcode/evac.svg",
+      iconSize: [39,39],
+      popupAnchor: [0.5, -15]
+    })}
+  ).addTo(map);
+
+  marker.bindPopup(`
+    <div class = "evac-marker-header">EVACUATION CENTER</div>
+    <div style = "text-align:center;">
+      <b>${center.name}</b>
+      <br>Location: ${center.latitude}, ${center.longitude}
+    </div>`);
+});
 
 addListeners();
 
@@ -235,15 +220,14 @@ function onMapClick(e) {
 
   // This is the popup for when the user clicks on a spot on the map
   var popupContent = `
-      <div class="partner-geolocation">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
-            <path d="M12 2C8.13 2 5 5.13 5 9C5 14.25 12 22 12 22C12 22 19 14.25 19 9C19 5.13 15.87 2 12 2ZM12 11.5C11.337 11.5 10.7011 11.2366 10.2322 10.7678C9.76339 10.2989 9.5 9.66304 9.5 9C9.5 8.33696 9.76339 7.70107 10.2322 7.23223C10.7011 6.76339 11.337 6.5 12 6.5C12.663 6.5 13.2989 6.76339 13.7678 7.23223C14.2366 7.70107 14.5 8.33696 14.5 9C14.5 9.66304 14.2366 10.2989 13.7678 10.7678C13.2989 11.2366 12.663 11.5 12 11.5Z" fill="#91C9DB"/>
-            </svg>
-            ${lat} + ${lng}
-            <br>
-      </div>
-    <button id="mainButton" class="addButton p-5" data-lat="${lat}" data-lng="${lng}">Add Household</button>
-    `;
+    <div class="partner-geolocation">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+          <path d="M12 2C8.13 2 5 5.13 5 9C5 14.25 12 22 12 22C12 22 19 14.25 19 9C19 5.13 15.87 2 12 2ZM12 11.5C11.337 11.5 10.7011 11.2366 10.2322 10.7678C9.76339 10.2989 9.5 9.66304 9.5 9C9.5 8.33696 9.76339 7.70107 10.2322 7.23223C10.7011 6.76339 11.337 6.5 12 6.5C12.663 6.5 13.2989 6.76339 13.7678 7.23223C14.2366 7.70107 14.5 8.33696 14.5 9C14.5 9.66304 14.2366 10.2989 13.7678 10.7678C13.2989 11.2366 12.663 11.5 12 11.5Z" fill="#91C9DB"/>
+          </svg>
+          ${lat} + ${lng}
+          <br>
+    </div>
+    <button id="mainButton" class="addButton p-5" data-lat="${lat}" data-lng="${lng}">Add Household</button>`;
 
   popup.setLatLng(e.latlng).setContent(popupContent).openOn(map);
 
